21fb78e72086db6f7bbf2e5f2c1481ae86c703b8
Daniel-Dias <daniel.dias007@cs.unipe.edu.br>
2026-04-07 13:29:23 -0300

    WIP:alterações em agenda

--- services/agendaService/src/modules/Consulta/Agenda/Domain/Entities/agendaEntity.py
+++ services/agendaService/src/modules/Consulta/Agenda/Domain/Entities/agendaEntity.py
@@ -1,8 +1,13 @@
-
 class Agenda:
     def __init__(self, id: IdAgenda, name: str, consultas: list[Consulta] = [], eventos: list[Evento] = [], pacientes: list[Paciente] = [], profissionais: list[Profissional] = [], especialidades: list[Especialidade] = [], horarios: list[Horario] = []):
         self.id = id
         self.name = name
+        self.consultas = consultas
+        self.eventos = eventos
+        self.pacientes = pacientes
+        self.profissionais = profissionais
+        self.especialidades = especialidades
+        self.horarios = horarios
 
     def __repr__(self): 
         return f"Agenda(id={self.id}, name={self.name})"
