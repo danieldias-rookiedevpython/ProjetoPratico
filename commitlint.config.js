c8805dd38064883f5554b011622c37a04fc7ebe7
Daniel-Dias <daniel.dias007@cs.unipe.edu.br>
2026-05-30 21:57:12 -0300

    fix: configuracao de padronizacao de commits

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'api',
+        'ui',
+        'docs',
+        'tests',
+        'agendaService',
+        'notificacaoService',
+        'userService',
+        'auth'
+      ]
+    ],
+    'subject-max-length': [2, 'always', 72],
+    'subject-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore'
+      ]
+    ]
+  }
+};
